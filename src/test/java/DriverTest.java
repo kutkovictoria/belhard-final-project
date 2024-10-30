a09656bafafae4d7cab0c4680a17e42030ec61d4
kutkovictoria <kutkovictoria@gmail.com>
2024-10-30 14:47:58 +0300

    update pom.xml, add DriverTest.java with click10thLink().

---
+++ src/test/java/DriverTest.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class DriverTest {
+    @Test
+    public void click10thLink() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com");
+        WebElement expected10thLink = driver.findElement(By.linkText("Disappearing Elements"));
+        List<WebElement> allLinks = driver.findElements(By.tagName("a"));
+        allLinks.get(9).click();
+
+        Assert.assertEquals(allLinks.get(9), expected10thLink);
+
+        driver.quit();
+    }
+}
