b2ba8779f61a8f7a584bc859112c626b5ce1a54f
kutkovictoria <kutkovictoria@gmail.com>
2024-11-09 19:43:37 +0300

    add LinksTest.java

--- src/test/java/DriverTest.java
+++ src/test/java/DriverTest.java
@@ -5,19 +5,26 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.List;
 
 public class DriverTest {
     @Test
     public void click10thLink() {
         WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
         driver.get("https://the-internet.herokuapp.com");
-        WebElement expected10thLink = driver.findElement(By.linkText("Disappearing Elements"));
+
+        WebElement tenthLinkElement = driver.findElement(By.linkText("Disappearing Elements"));
+        String expectedTitle = tenthLinkElement.getText();
         List<WebElement> allLinks = driver.findElements(By.tagName("a"));
         allLinks.get(9).click();
+        WebElement actualTitleElement = driver.findElement(By.xpath("//div[@id='content']/div[@class = 'example']/h3"));
+        String actualTitle = actualTitleElement.getText();
 
-        Assert.assertEquals(allLinks.get(9), expected10thLink);
-
+        Assert.assertEquals(actualTitle, expectedTitle);
         driver.quit();
     }
 }
